f1d39a21dd4d726adaeb480bc767c876f7b69820
Me-Yan <yanyanghong_work@163.com>
2018-08-31 00:13:07 +0800

    add code

---
+++ documents/DB/init_DATA.sql
@@ -0,0 +1,5 @@
+INSERT INTO cf_role(roleName_m, enabled_c, crtOn_dt, crtBy_m) VALUES('ROLE_SUP', 'Y', NOW(), 'System');
+INSERT INTO cf_resource(resType_x, resPath_x, resDesc_x, enabled_c, crtOn_dt, crtBy_m) VALUES('HOME', '/dashboard', 'Home Page', 'Y', NOW(), 'System');
+INSERT INTO cf_role2res(roleId_n, resId_n, crtOn_dt, crtBy_m) VALUES(1, 1, NOW(), 'System');
+INSERT INTO cf_user(username_m, password_x, enabled_c, crtOn_dt, crtBy_m) VALUES('Admin', '$2a$11$oGpHm0cUXvtUOupwtlgL8./6EXUa3sOTuycMAc6DdiB0tkl7l4ula', 'Y', NOW(), 'System');
+INSERT INTO cf_user2role(userId_n, roleId_n, crtOn_dt, crtBy_m) VALUES(1, 1, NOW(), 'System');
---
+++ me-cf/cf-core/src/main/java/com/me/inner/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.me.inner.constant;
+
+/**
+ * Created by Me on 2018/8/30.
+ */
+public class Constant {
+
+    public interface User_Role {
+        String ROLE_SUPER = "ROLE_SUP";
+        String ROLE_NOR = "ROLE_NOR";
+    }
+}
